6284acfbb41e6464a0938e2d26bce9c3e4de7d28
Rachel <rachtuch00@gmail.com>
2022-12-14 15:42:56 -0500

    commit tree 3aa98d995a37783788ffa82e7d2a99d87569953b

--- index.js
+++ index.js
@@ -1,1 +1,32 @@
 // Code your solutions in this file
+for (let age = 30; age < 40; age++) {
+    console.log(`I'm ${age} years old. Happy birthday to me!`);
+}
+
+const gifts = ["teddy bear", "drone", "doll"];
+
+function wrapGifts(gifts) {
+    for (let i=0; i < gifts.length; i++) {
+        console.log(`Wrapped ${gifts[i]} and added a bow!`);
+        debugger;
+    }
+    return gifts;
+}
+wrapGifts(gifts);
+
+
+function writeCards(names, event) {
+    const messages = []
+        for (let i = 0; i < names.length; i++) {
+            const message = `Thank you, ${names[i]}, for the wonderful ${event} gift!`;
+            messages.push(message);
+        }
+        return messages;
+    }
+   writeCards(names);
+ 
+function countDown(number) {
+    for (let i=4; i<10; i++)
+    console.log(Math.random(number));
+}
+countDown(number);
